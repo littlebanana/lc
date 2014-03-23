ed60bb7d9ffd70d636d4157a6ed434790f3be5fb
littlebanana <jinpengchong@hotmail.com>
2014-03-23 00:46:33 -0400

    3rd

--- MaxPointsOnALine/MaxPointsOnALine.cpp
+++ MaxPointsOnALine/MaxPointsOnALine.cpp
@@ -1,124 +1,168 @@
+// Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 
-struct Point
-{
+struct Point {
     int x;
     int y;
     Point() : x(0), y(0) {}
     Point(int a, int b) : x(a), y(b) {}
 };
 
-class Solution
-{
+class Solution {
     public:
-        class SlopeComparator
+        static double slope(const Point &p1, const Point &p2)
+        {
+            if (p1.x == p2.x && p1.y == p2.y)
+            {
+                return -numeric_limits<double>::infinity();
+            }
+            if (p1.x == p2.x)
+            {
+                return numeric_limits<double>::infinity();
+            }
+            return (double)(p1.y - p2.y)/(double)(p1.x - p2.x);
+        }
+
+        // use hash table to record the slopes and counts
+        int maxPoints1(vector<Point> &points) {
+            int n = points.size();
+            int maxP = 0;
+            unordered_map<double,int> sets;
+            for (int i = 0; i < n; i++)
+            {
+                int thisP = 1;
+                int sameP = 0;
+                sets.clear();
+                for (int j = i+1; j < n; j++)
+                {
+                    double s = Solution::slope(points[i], points[j]);
+                    if (s == -numeric_limits<double>::infinity())
+                    {
+                        sameP++;
+                        continue;
+                    }
+
+                    auto it = sets.find(s);
+                    if (it != sets.end())
+                    {
+                        it->second++;
+                        thisP = max(thisP, it->second);
+                    }
+                    else
+                    {
+                        sets.insert(pair<double,int>(s,2));
+                        thisP = max(thisP, 2);
+                    }
+                }
+                thisP += sameP;
+                maxP = max(maxP, thisP);
+            }
+            return maxP;
+        }
+
+        class MyComparator
         {
             public:
-                SlopeComparator(Point p) : pivot(p) {}
+                MyComparator(const Point &p) : pivot(Point(p.x, p.y)) {}
 
-                bool operator() (const Point &lhs, const Point &rhs)
+                bool operator() (const Point &p1, const Point &p2)
                 {
-                    if (lhs.x == pivot.x && rhs.x == pivot.x)     // vertical
-                    {
-                        return false;
-                    }
-                    if (lhs.x == pivot.x)
-                    {
-                        double r_slope = (double)(rhs.y - pivot.y)/(rhs.x - pivot.x);
-                        return (r_slope < 0);
-                    }
-                    if (rhs.x == pivot.x)
-                    {
-                        double l_slope = (double)(lhs.y - pivot.y)/(lhs.x - pivot.x);
-                        return (l_slope >= 0);
-                    }
-                    double l_slope = (double)(lhs.y - pivot.y)/(lhs.x - pivot.x);
-                    double r_slope = (double)(rhs.y - pivot.y)/(rhs.x - pivot.x);
-                    if (l_slope >= 0 && r_slope >= 0)
-                    {
-                        return (l_slope < r_slope);
-                    }
-                    if (l_slope < 0 && r_slope < 0)
-                    {
-                        return (l_slope > r_slope);
-                    }
-                    return (l_slope > r_slope);
+                    double s1 = Solution::slope(pivot, p1);
+                    double s2 = Solution::slope(pivot, p2);
+                    return (s1 < s2);
                 }
 
             private:
                 Point pivot;
         };
 
-        int maxPoints(vector<Point> &points) {
-            int N = points.size();
-            if (N < 2)
-            {
-                return N;
-            }
-            int maxcount = 2;
-            for (int i = 0; i < N-1; i++)
-            {
-                // the pivot in the outer loop is points[i]
-                unordered_map<double,int> slopetab;
-                int thiscount = 0;
-                int vcount = 0;
-                int same = 1;
-                for (int j = i+1; j < N; j++)
+        class MyEquator
+        {
+            public:
+                MyEquator(const Point &p) : pivot(Point(p.x, p.y)) {}
+
+                bool operator() (const Point &p1, const Point &p2)
                 {
-                    if (points[j].x == points[i].x && points[j].y == points[i].y)
+                    double s1 = Solution::slope(pivot, p1);
+                    double s2 = Solution::slope(pivot, p2);
+                    return (s1 == s2);
+                }
+
+            private:
+                Point pivot;
+        };
+
+        int maxPoints2(vector<Point> &points) {
+            int n = points.size();
+            int maxP = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int thisP = 1;
+                int sameP = 0;
+
+                MyComparator comp(points[i]);
+                sort(points.begin()+i+1, points.end(), comp);
+
+                int j = i+1;
+                while (j < n && Solution::slope(points[j], points[i]) == -numeric_limits<double>::infinity())
+                {
+                    j++;
+                    sameP++;
+                }
+                if (j < n)
+                {
+                    MyEquator equal(points[i]);
+                    j++;
+                    int prevP = 2;
+                    while (j < n)
                     {
-                        same++;
-                    }
-                    else if (points[j].x == points[i].x) // vertical line
-                    {
-                        vcount++;
-                    }
-                    else
-                    {
-                        double slope = (double)(points[j].y - points[i].y)/(points[j].x - points[i].x);
-                        cout << slope << endl;
-                        unordered_map<double,int>::iterator it = slopetab.find(slope);
-                        if (it != slopetab.end())
+                        if (equal(points[j-1], points[j]))
                         {
-                            it->second++;
-                            thiscount = max(it->second, thiscount);
+                            prevP++;
                         }
                         else
                         {
-                            slopetab.insert(pair<double,int>(slope,1));
-                            thiscount = max(1, thiscount);
+                            thisP = max(thisP, prevP);
+                            prevP = 2;
                         }
+                        j++;
                     }
+                    thisP = max(thisP, prevP);
                 }
-                cout << "same = " << same << endl;
-                cout << "vcount = " << vcount << ", thiscount = " << thiscount << endl;
-                thiscount = max(vcount, thiscount) + same;
-                cout << "thiscount = " << thiscount << endl;
-                maxcount = max(thiscount, maxcount);
+                thisP += sameP;
+                maxP = max(maxP, thisP);
             }
-            return maxcount;
+            return maxP;
         }
 };
 
 int main()
 {
-    vector<Point> points;
-    points.push_back(Point(84,250));
-    points.push_back(Point(0,0));
-    points.push_back(Point(1,0));
-    points.push_back(Point(0,-70));
-    points.push_back(Point(0,-70));
-    points.push_back(Point(1,-1));
-    points.push_back(Point(21,10));
-    points.push_back(Point(42,90));
-    points.push_back(Point(-42,-230));
+    vector<Point> points(7);
+    points[0].x = -4;
+    points[0].y = -4;
+    points[1].x = -9;
+    points[1].y = -651;
+    points[2].x = -4;
+    points[2].y = 4;
+    points[3].x = -8;
+    points[3].y = -582;
+    points[4].x = -3;
+    points[4].y = 3;
+    points[5].x = -9;
+    points[5].y = -651;
+    points[6].x = 9;
+    points[6].y = 651;
 
     Solution solu;
-    cout << solu.maxPoints(points) << endl;
+    cout << solu.maxPoints1(points) << endl;
 
     return 0;
 }
